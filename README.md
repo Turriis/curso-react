de8fe8a62c6f3f64ea05e359ef8cac0098eaa06b
Turriis <arturocecena@hotmail.com>
2023-02-28 15:50:20 -0600

    Archivo principal y practica con var, let y const

---
+++ README.md
@@ -0,0 +1,1 @@
+# React course teached by Hola mundo, here I would upload my practices for myself an for anyone that can get something good of it. Thanks for reading
---
+++ index.js
@@ -0,0 +1,16 @@
+var miVariable = 'Mi variable'
+
+console.log(miVariable)
+
+/*
+    La diferencia entre var y let es que
+        var: Se crea y declara la variable desde el comienzo del código.
+        let: Se crea y declara la variable desde la línea en donde se creó.
+    
+    Si se trata de mostrar el resultado con console.log(), si se usa let se tien que
+    poner despues de la línea escrita, si se pone antes dará error ya que js no lo detecta antes porque no es
+    tipo var.
+
+        const: No se puede cambiar el valor por ningun motivo y si se quiere mostrar debe ser despues
+        de escribir la línea (como en el caso de let)
+*/
